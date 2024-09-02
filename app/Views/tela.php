55ab2b0881052a28aad2c0212af68586e0f4d92b
Alcatrax1 <pedro.2022300811@aluno.iffar.edu.br>
2024-09-02 20:43:59 -0300

    1.2

--- app/Views/tela.php
+++ app/Views/tela.php
@@ -1,52 +1,44 @@
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sport Connect</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="./tela.css">
+  <title>Document</title>
 </head>
 <style>
-
-    body{
-background-color: #c0c0c0;
-margin: 0;
-color: #333;
-padding: 20px;
-
-    }
-    .caixa{
-       width: 750px;
-       height: 100px;
-       background-color:transparent;
-        border: transparent;
-    }
-.letra{
-font-family: 'Graphik web', sans-serif;
-display: flex;
-color: #848484;
-text-decoration: none;
-margin-left: 1500px;
-margin-top: 20px;
-
+body {
+    background-color: white;
+    margin: 0;
 }
-.caixa{
-width: 100%;;
-height: auto;
-color: black;
 
+.caixa-superior {
+    width: 100%;
+    height: 30px;
+    background-color: black;
+    opacity: 0.7;
+    position: fixed;
+    top: 0;
+    z-index: 100;
 }
- 
+
+.caixa-superior input[type="text"] {
+    
+}
+
+
+@media (max-width: 768px) {
+    .caixa-superior {
+        height: 50px;
+    }
+
+    .caixa-superior input[type="text"] {
+        font-size: 14px;
+    }
+}
+
 </style>
 <body>
-
-<nav> <div class="caixa">
-
-    
-  <h1>Sport Connect</h1>
-  <a href="#" class="letra"> Login</a>
-<img src="PPI2K24\public/bola.mp4">
-</div>  </nav>
-
+  <nav class="caixa-superior"><div></div></nav>
 </body>
 </html>
