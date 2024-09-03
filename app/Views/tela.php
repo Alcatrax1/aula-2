3dbf3e3a4c9d107af0616604bb95b55fa2fd3772
Alcatrax1 <pedro.2022300811@aluno.iffar.edu.br>
2024-09-02 21:56:01 -0300

    1.3

--- app/Views/tela.php
+++ app/Views/tela.php
@@ -9,18 +9,27 @@
 <style>
 body {
     background-color: white;
+   padding: 10px; /*espaçamentos*/
+    font-family: Arial, sans-serif;
     margin: 0;
 }
 
 .caixa-superior {
     width: 100%;
-    height: 30px;
+    height: 65px;
     background-color: black;
     opacity: 0.7;
     position: fixed;
     top: 0;
     z-index: 100;
+    padding: 0;
+    display: flex;
+    left: 0;
+
+    /* justify-content: left;  Centraliza horizontalmente */
+    align-items: center;  /* Centraliza verticalmente */
 }
+
 
 .caixa-superior input[type="text"] {
     
@@ -36,9 +45,35 @@ body {
         font-size: 14px;
     }
 }
+#esportes{
+    background-color: green;
+    width: 105px;
+    height: 65px;
+    /* Centraliza vertical e horizontalmente */
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-left: 300px; 
+    text-align: center; 
+}
+.imagem-fut{
+display: flex;
+height: auto;
+width: 100%;
+justify-content: center;
+align-items: center ;
 
+
+
+}
 </style>
 <body>
-  <nav class="caixa-superior"><div></div></nav>
+  <nav class="caixa-superior"><div> 
+<h1 id="esportes">Esportes</h1>
+
+  </div>
+
+  <img class="imagem-fut" src="public/futebol.jpg">
+</nav>
 </body>
 </html>
